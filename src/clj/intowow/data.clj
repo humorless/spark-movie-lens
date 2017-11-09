626cc0eb36ac8a633d591580041f2275668f1a52
Laurence Chen <humorless@gmail.com>
2017-11-09 23:46:59 +0800

    add genre read from file function and write to db function

--- src/clj/intowow/data.clj
+++ src/clj/intowow/data.clj
@@ -26,6 +26,23 @@
          (map line->item-tuple)
          (into {}))))
 
+(defn line->genre-tuple [line]
+  (let [line-list (s/split line #"\|")
+        genre (drop 5 line-list)
+        id (first line-list)]
+    (->> (cons id genre)
+         (map to-long)
+         (zipmap [:id :unknown :action :animation :children :comedy :crime :documentary :drama :fantasy :film_noir :horror :musical :mystery :romance :sci_fi :thriller :war :western]))))
+
+(defn load-genres
+  "return vector as:
+ [{:western 0, :fantasy 0, :children 1, :animation 0, :horror 0, :mystery 0, :unknown 0, :musical 0, :romance 0, :war 0, :drama 0, :sci_fi 0, :documentary 0, :id 1, :comedy 1, :thriller 0, :action 0, :film_noir 0, :crime 1}, ... ] "
+  [path]
+  (with-open [rdr (io/reader (io/resource path))]
+    (->> (line-seq rdr)
+         (map line->genre-tuple)
+         (into []))))
+
 (defn item->name [file]
   (let [items (load-items file)]
     (fn [{:keys [id]}]
